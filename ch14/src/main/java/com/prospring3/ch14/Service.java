3192b947520108ada344d937e7849ccbd3e2a12d
hjkim <hjkim@doctorsoft.co.kr>
2014-01-27 20:41:02 +0900

    add ch14 타입 변환과 포메팅을 활용한 유효성 검증

---
+++ ch14/src/main/java/com/prospring3/ch14/Service.java
@@ -0,0 +1,7 @@
+package com.prospring3.ch14;
+
+public interface Service {
+	
+	String getMessage();
+
+}
---
+++ ch14/src/test/java/com/prospring3/ch14/ExampleServiceTests.java
@@ -0,0 +1,14 @@
+package com.prospring3.ch14;
+
+import com.prospring3.ch14.ExampleService;
+import junit.framework.TestCase;
+
+public class ExampleServiceTests extends TestCase {
+
+	private ExampleService service = new ExampleService();
+	
+	public void testReadOnce() throws Exception {
+		assertEquals("Hello world!", service.getMessage());
+	}
+
+}
